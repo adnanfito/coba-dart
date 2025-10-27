6ba44162663dab3fbec7d10155713c7c85c1c26d
Adnan Fito <112043828+adnanfito@users.noreply.github.com>
2025-10-27 10:12:25 +0700

    first commit

--- todo.dart
+++ todo.dart
@@ -1,10 +1,8 @@
 import 'dart:io';
 
-// List untuk menyimpan semua tugas
 List<String> tasks = [];
 
 void main() {
-  // Loop utama aplikasi akan terus berjalan sampai pengguna memilih untuk keluar
   while (true) {
     print('\n--- APLIKASI TODO LIST ---');
     print('1. Tambah Tugas');
@@ -14,10 +12,8 @@ void main() {
     print('--------------------------');
     stdout.write('Pilih menu: ');
 
-    // Membaca input dari pengguna
     String? choice = stdin.readLineSync();
 
-    // Memproses pilihan pengguna
     switch (choice) {
       case '1':
         addTask();
@@ -29,7 +25,6 @@ void main() {
         deleteTask();
         break;
       case '4':
-        // Menghentikan aplikasi
         print('Terima kasih! Sampai jumpa.');
         return;
       default:
@@ -38,12 +33,10 @@ void main() {
   }
 }
 
-/// Fungsi untuk menambahkan tugas baru
 void addTask() {
   stdout.write('Masukkan nama tugas baru: ');
   String? task = stdin.readLineSync();
 
-  // Memastikan input tidak kosong
   if (task != null && task.isNotEmpty) {
     tasks.add(task);
     print(
@@ -56,23 +49,20 @@ void addTask() {
   }
 }
 
-/// Fungsi untuk menampilkan semua tugas yang ada
 void viewTasks() {
   if (tasks.isEmpty) {
     print('Daftar tugas masih kosong.');
   } else {
     print('\n--- DAFTAR TUGAS ---');
     for (int i = 0; i < tasks.length; i++) {
-      // Menampilkan nomor dan nama tugas
       print('${i + 1}. ${tasks[i]}');
     }
     print('----------------------');
   }
 }
 
-/// Fungsi untuk menghapus tugas dari daftar
 void deleteTask() {
-  viewTasks(); // Tampilkan dulu semua tugas agar pengguna tahu nomornya
+  viewTasks();
   if (tasks.isNotEmpty) {
     stdout.write('Masukkan nomor tugas yang ingin dihapus: ');
     String? input = stdin.readLineSync();
@@ -81,7 +71,6 @@ void deleteTask() {
       try {
         int index = int.parse(input) - 1;
 
-        // Memeriksa apakah nomor yang dimasukkan valid
         if (index >= 0 && index < tasks.length) {
           String removedTask = tasks.removeAt(index);
           print(
